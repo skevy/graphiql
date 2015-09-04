c7c7b817f1f40b34c9cf9d088af8be8dbd553d75
Hyohyeon Jeong <asiandrummer@gmail.com>
2015-09-04 11:49:13 +0900

    quick fix to remove unnecessary code and correctly bind the context

--- src/components/DocExplorer.js
+++ src/components/DocExplorer.js
@@ -65,7 +65,6 @@ export class DocExplorer extends React.Component {
         {__html: this.marked(text)}
       }
     />;
-    return this.marked(text);
   }
 
   _getTypeLink(type) {
@@ -111,7 +110,7 @@ export class DocExplorer extends React.Component {
   }
 
   _renderTypeValues(type) {
-    var _renderMarkdown = this._renderMarkdown;
+    var _renderMarkdown = this._renderMarkdown.bind(this);
     function renderValue(value) {
       return (
         <div className="doc-category-item">
